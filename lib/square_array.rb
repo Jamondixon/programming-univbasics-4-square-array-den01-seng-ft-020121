84ef0a7d5cb29e570a5515600f1d5b606ff7ea0f
Jamon Dixon <jamonadixon@gmail.com>
2021-01-15 19:25:41 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -1,9 +1,9 @@
 def square_array(array)
   new_array = []
 
-  array.each[counter] do |integer|
+  array.each.times do |integer|
   new_array << integer ** 2
 
  end
- new_array
+  return new_array
 end
